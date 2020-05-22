4673ef7e10ef72d2c91161d0f55f9aeac4427107
Johanna Roll <johanna-roll@outlook.de>
2020-05-22 11:57:39 +0200

    Loopen durch einzelne Einträge - Ort des Todes

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log(DATA.data);
+console.log("Datensatz", DATA);
+console.log("Daten",DATA["data"])
+
+for (let key in DATA) {
+    //erste Ebene
+    if (!DATA.hasOwnProperty(key)) continue;
+    let obj = DATA[key];
+    //zweite Ebene - einzelne Datensätze
+    for (let i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+
+        console.log("Eintrag", i)
+        // console.log(obj[prop]); 
+        let PoD = obj[i][46]; //Place of Death
+        // console.log(typeof PoD); //object
+        console.log("Ort des Todes");
+        console.log(PoD);
+        // console.log("Lat", PoD[1]) // Fehler: "PoD is undefined" (auch bei PoD["1"])
+        // console.log("Lng", PoD[2])
+    }
+
+};
