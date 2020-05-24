bab7c04e5228e3e06e71005fe84b6b76b09709ad
Johanna Roll <johanna-roll@outlook.de>
2020-05-25 01:08:47 +0200

    DrawAccidents - alle Fälle mit Marker

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 //Map Setup
 let startLayer = L.tileLayer.provider("Esri.WorldStreetMap");
 
+let overlay = {
+    drugaccidents: L.featureGroup()
+};
+
 let map = L.map("map", {
     center: [41.7625, -72.674167], //Hartford
     zoom: 8,
     layers: [
-        startLayer
+        startLayer,
+        overlay.drugaccidents
     ]
 });
-
-let overlay = {
-    drugaccidents: L.featureGroup() 
-};
 
 L.control.layers({
     "Esri.WorldStreetMap": L.tileLayer.provider("Esri.WorldStreetMap"),
@@ -22,15 +23,24 @@ L.control.layers({
 }).addTo(map);
 
 //Data
-console.log("Datensatz", DATA);
-console.log("Daten", DATA["data"])
+// console.log("Datensatz", DATA);
+// console.log("Daten", DATA["data"])
 
-for (let index in DATA.data) {
-    if (!DATA.data.hasOwnProperty(index)) continue;
-    let element = DATA.data[index];
-    // console.log(element);
-    let PoD = element[46] //GeoInfo Sterbeort
-    // console.log("lat", element[46][1]);
-    console.log("Eintrag:", index);
-    console.log("Sterbeort:", "(lat)", PoD[1], "--- (lng)", PoD[2])
+let drawAccidents = function () {
+    let AccData = DATA.data;
+
+    for (let index in AccData) {
+        if (!AccData.hasOwnProperty(index)) continue;
+        let element = AccData[index];
+        // console.log(element);
+        let PoD = element[46] //GeoInfo Sterbeort
+        // console.log("lat", element[46][1]);
+        // console.log("Eintrag:", index);
+        // console.log("Sterbeort:", "(lat)", PoD[1], "--- (lng)", PoD[2])
+        let lat = PoD[1];
+        let lng = PoD[2];
+        let mrk = L.marker([lat, lng]).addTo(overlay.drugaccidents);
+    }
 };
+
+drawAccidents();
