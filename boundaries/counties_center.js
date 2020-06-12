e203005e2951787ae598832adb7f3e6718594975
Johanna Roll <johanna-roll@outlook.de>
2020-06-12 13:57:25 +0200

    County Center Coordinaten

---
+++ boundaries/counties_center.js
@@ -0,0 +1,10 @@
+const county_center = [
+    ["HARTFORD", 41.81, -72.73],
+    ["NEW HAVEN", 41.35, -72.9],
+    ["FAIRFIELD", 41.23, -73.37],
+    ["NEW LONDON", 41.47, -72.1],
+    ["LITCHFIELD", 41.79, -73.24],
+    ["MIDDLESEX", 41.44, -72.52],
+    ["WINDHAM", 41.83, -71.99],
+    ["TOLLAND", 41.85, -72.33],
+]
