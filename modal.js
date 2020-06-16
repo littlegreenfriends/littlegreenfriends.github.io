5890c23ba887cbeaedfd1aa8f04de24dddfb0c00
gubbllukas <l.endres90@googlemail.com>
2020-06-16 21:25:31 +0200

    Abwandlung Modal von ID ind Class für mehrere Bilder pro Seite

--- modal.js
+++ modal.js
@@ -1,17 +1,24 @@
-// Skript für Modal Bildvergrößerungen
+// Skript für Modal Bildvergrößerungen (für mehrere Bilder abgewandelt)
 // Get the modal
 let modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.getElementById("myImg");
+// Muss nur einmal ausgeführt werden
 let modalImg = document.getElementById("img01");
 let captionText = document.getElementById("caption");
-img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+
+//Umwandlung nötig, da id nur ein Element zulässt, wir aber mehrere Bilder haben wollen
+//Klasse enthält mehrere Elemente, für jedes Element soll die onclick-Funktion ausgeführt werden
+let array = document.getElementsByClassName('zoomImg');
+for (let i = 0; i < array.length; i++) {
+    array[i].onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
 }
 
+// Wieder nur einmal auszuführen
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
